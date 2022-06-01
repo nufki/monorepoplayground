d1a41fda4e61c433adaa522e8c5ba80d80a7e2bd
nuferkei <kei.nufer>
2022-06-01 16:08:22 +0200

    save

--- libs/social/src/lib/containers/create-comment/create-comment.component.ts
+++ libs/social/src/lib/containers/create-comment/create-comment.component.ts
@@ -1,22 +1,64 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { filter, Observable, switchMap } from 'rxjs';
 import { createComment } from '../../+state/post.actions';
-import { PostEntity } from '../../+state/post.models';
-import { selectPost } from '../../+state/post.selectors';
+import { CommentEntity, PostEntity } from '../../+state/post.models';
+import { selectComments, selectPost } from '../../+state/post.selectors';
 
 @Component({
   selector: 'united-create-comment',
   templateUrl: './create-comment.component.html',
   styleUrls: ['./create-comment.component.scss'],
 })
-export class CreateCommentComponent {
+export class CreateCommentComponent implements OnChanges {
   post$: Observable<PostEntity | undefined> = this.store.select(selectPost);
+
   commentText = '';
+  @ViewChild('commentArea') $commentArea?: ElementRef<HTMLTextAreaElement>;
   @Output() inputFocus = new EventEmitter<boolean>();
   showKeyboard = false;
+  @Input() initalFocus = false;
+  @Input() comments: CommentEntity[] | undefined;
 
   constructor(private readonly store: Store) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('changes: ', changes);
+    if (changes['comments'].currentValue) {
+      this.comments = changes['comments'].currentValue;
+      console.log('comments (from on changes): ', this.comments);
+      if (this.initalFocus) {
+        setTimeout(() => {
+          this.$commentArea?.nativeElement.focus();
+        }, 500);
+      }
+    }
+  }
+
+  /***************************************************************************
+   * Set initial Focus
+   ***************************************************************************/
+  // ngAfterViewInit(): void {
+  //   console.log('initalFocus: ', this.initalFocus);
+  //   console.log('comments: ', this.comments);
+
+  //   if (this.initalFocus) {
+  //     setTimeout(() => {
+  //       this.$commentArea?.nativeElement.focus();
+  //     }, 500);
+  //   }
+  // }
 
   onCreateComment(post: PostEntity) {
     this.store.dispatch(
@@ -32,8 +74,11 @@ export class CreateCommentComponent {
   }
 
   onFocusOutEvent(event: any) {
+    // Delay the focus out to allow the comment creation (since the flag: showKeyboard will clear the post button)
+    setTimeout(() => {
+      this.showKeyboard = false;
+      this.inputFocus.emit(false);
+    }, 100);
     console.log('onFocusOutEvent');
-    this.inputFocus.emit(false);
-    this.showKeyboard = false;
   }
 }
